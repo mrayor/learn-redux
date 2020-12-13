68e3e86e5f04a7d76ba48ca9e1f21439426489fd
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-12-13 18:53:00 +0100

    destructure action

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -11,33 +11,33 @@ const initialState = {
   contact: {}
 };
 
-export default function(state = initialState, action) {
-  switch (action.type) {
+export default function (state = initialState, { type, payload }) {
+  switch (type) {
     case GET_CONTACTS:
-      return { ...state, contacts: action.payload };
+      return { ...state, contacts: payload };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
+          contact => contact.id !== payload
         )
       };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [action.payload, ...state.contacts]
+        contacts: [payload, ...state.contacts]
       };
     case GET_CONTACT:
       return {
         ...state,
-        contact: action.payload
+        contact: payload
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map(contact =>
-          contact.id === action.payload.id
-            ? (contact = action.payload)
+          contact.id === payload.id
+            ? (contact = payload)
             : contact
         )
       };
